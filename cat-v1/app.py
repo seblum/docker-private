b84416316256474c856ebdaae5c984e11463b255
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2022-08-13 10:17:22 +0200

    added cat-v1 & github actions to private repo

---
+++ cat-v1/app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# @app.route('/how are you')
+# def hello():
+#     return 'I am good, how about you?'
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port='5000')
